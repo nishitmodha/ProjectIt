551ae3cab0237e813e0518a0760134ea54c5b880
nishitmodha <nishitmodha30@gmail.com>
2021-01-25 17:30:25 +0530

    Added Email Confirmation for sign up using Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 	
-  devise_for :users
+  	as :user do
+    	put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+  	end
+
+   devise_for :users, controllers: {
+  	registrations: 'registrations',
+  	confirmations: 'confirmations'
+  }
+
   root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
