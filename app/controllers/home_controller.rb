fa1e17f376fc27eef3878589aabb68b9cc6ba902
nishitmodha <nishitmodha30@gmail.com>
2021-01-26 12:24:22 +0530

    Modified Controllers and dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+  		@teams = Team.where('id = ?', current_user.team_id)
+  		@projects = Project.where('team_id = ?', current_user.team_id)
+  	end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
     belongs_to :team
     belongs_to :user
 
-    accept_nested_attributes_for :team
+    accepts_nested_attributes_for :team
 end
